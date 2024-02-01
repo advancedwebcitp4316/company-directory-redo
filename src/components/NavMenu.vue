1e21d6d3fd8f2343742310460e795aebf3af4caf
advancedwebcitp4316@gmail.com <advancedwebcitp4316@gmail.com>
2024-02-01 13:01:22 -0600

    added welcome message to login

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,25 +1,27 @@
 <template>
-  <nav class="bg-slate-900 h-20 items-center text-white flex justify-center space-x-4">
-    <RouterLink :to="{name: 'Home'}">Home</RouterLink>
-    <RouterLink :to="{name: 'Other'}">Other</RouterLink>
-    <RouterLink :to="{name: 'LoginPage'}">Login</RouterLink>
-    <RouterLink :to="{name: 'SettingsPage'}">settings</RouterLink>
+  <nav class="bg-slate-900 h-20 items-center text-white flex justify-center space-x-8">
+    <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
+    <RouterLink :to="{ name: 'Other' }">Other</RouterLink>
+
+
+    
+      <div v-if="isAuthenticated">
+        Welcome {{ user?.email }}
+        <RouterLink :to="{ name: 'SettingsPage' }">Settings</RouterLink>
+        <button @click="logout" class="rounded-md bg-red-500 px-4 py-2 mx-2 text-red-100 hover:bg-red-700">Logout</button>
+      </div>
+
+      <div v-else>
+        <RouterLink :to="{ name: 'LoginPage' }">Login</RouterLink>
+      </div>
+
   </nav>
 </template>
 
-<!-- <style lang="postcss" scoped>
-nav {
-  @apply flex justify-center space-x-4;
+<script setup>
 
-  & .router-link-active {
-    @apply underline underline-offset-4;
-  }
-}
-</style> -->
+import { useAuth } from '../composables/useAuth'
+const { isAuthenticated, logout, user } = useAuth()
 
-<style lang="postcss">
-nav .router-link-active {
-  @apply underline underline-offset-4;
-}
 
-</style>
+</script>
