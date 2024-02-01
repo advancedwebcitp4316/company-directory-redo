4985eb813aa6170932f61f376c9091496ba64637
advancedwebcitp4316@gmail.com <advancedwebcitp4316@gmail.com>
2024-02-01 11:57:47 -0600

    style for form done

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,14 +1,14 @@
 <template>
-    <form @submit.prevent>
-        <div>
-            <label>Username</label>
-            <input v-model="username" type="text" placeholder="Username">
+    <form class="min-h-screen m-10" @submit.prevent>
+        <div class="mb-5">
+            <label for="username" class="block mb-2 font-bold">Username</label>
+            <input id="username" v-model="username" type="text" placeholder="Username">
         </div>
         <div>
-            <label>Password</label>
-            <input v-model="password" type="password" placeholder="Password">
+            <label for="password" class="block mb-2 font-bold">Password</label>
+            <input id="password" v-model="password" type="password" placeholder="Password">
         </div>
-        <button @click="logUserIn">Login</button>
+        <button @click="logUserIn" class=" mt-4 bg-green-500 px-4 py-2 hover:bg-green-800 hover:text-white">Login</button>
     </form>
 </template>
 
