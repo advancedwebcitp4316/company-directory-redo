53b9cde27144a2a68effad0efe005f4d8e3bd5da
advancedwebcitp4316@gmail.com <advancedwebcitp4316@gmail.com>
2024-02-02 14:05:21 -0600

    Navigation Guard complete

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import { useAuth } from './composables/useAuth'
+const {isAuthenticated} = useAuth()
 import HomePage from '@/views/HomePage.vue'
 import CardDetails from './views/CardDetails.vue'
 import LoginPage from './views/LoginPage.vue'
@@ -10,12 +11,29 @@ const routes = [
   { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
   { path: '/employees/:id', name: 'CardDetails', component: CardDetails },
   { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/settings', name: 'SettingsPage', component: SettingsPage },
+  { path: '/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+
+//registering a navigation guard that will be executed before each route navigation.
+
+router.beforeEach((to, _, next) => {
+  // Check if the destination route requires authentication
+  // and if the user is not authenticated
+  if (to.meta.requiresAuth && !isAuthenticated.value) {
+    // Redirect to the 'Login' route with a query parameter
+    // indicating the original route's full path
+    next({ name: 'Login', query: { redirect: to.fullPath } })
+  } else {
+    // Continue with the navigation if no authentication is required
+    // or if the user is authenticated
+    next()
+  }
 })
 
 export default router
